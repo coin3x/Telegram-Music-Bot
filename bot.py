82f22d31e30b06143a4e7962c1493257f2ef59be
RexTW <rexx0520@gmail.com>
2017-07-07 17:50:50 +0800

    醬子ㄋ

--- bot.py
+++ bot.py
@@ -83,7 +83,8 @@ def default(chat, message):
     msg1 = message["text"].split(" type:")
     msg1[0] = msg1[0].split(" ")
     def getremsg(source):
-        for i in range(0, len(source), 1):
+        msg_temp = source[0]
+        for i in range(1, len(source), 1):
             msg_temp = msgtemp + source[i]
         return (msg_temp)
     msg1[0] = getremsg(msg1[0])
