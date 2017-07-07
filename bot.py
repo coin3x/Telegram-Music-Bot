13455afdac9f91f7a4e9314ee76ff0d3eb604032
RexTW <rexx0520@gmail.com>
2017-07-08 00:37:59 +0800

    這個呢

--- bot.py
+++ bot.py
@@ -88,12 +88,12 @@ def default(chat, message):
     for k in range(len(msg1[0])):
         textA = textA + '(?=.*?' + msg1[0][k] + ")"
     textA = textA + '.*?/i'
-    msg1[0] = re.compile (textA, re.IGNORECASE)
+    final = re.compile (textA, re.IGNORECASE)
     logger.info(msg1[0])
     if (len(msg1) == 2):
-        return search_tracks(chat, msg1[0], typev=msg1[1])
+        return search_tracks(chat, final, typev=msg1[1])
     elif (len(msg1) == 1):
-        return search_tracks(chat, msg1[0])
+        return search_tracks(chat, final)
     else:
         logger.info("元素個數有問題RR")
         bot.send_message(os.environ.get("CHNID"),"元素個數有問題RRR")
