f265cc3c58f921263898176dfb6ce45f8f93a88a
RexTW <rexx0520@gmail.com>
2017-07-07 15:53:06 +0800

    該不會是mp3那邊出問題

--- database.py
+++ database.py
@@ -18,17 +18,14 @@ db = client.python
     ).sort([('score', {'$meta': 'textScore'})])'''
 
 def text_search(query,type='audio'):
-    if (type == 'mp3'):
-        type = 'mpeg'
     return db.tracks.find(
         {"$and":[
             {'mime_type':{'$regex':type, '$options':'i'}},
             {"$or":[
                 {'title':{'$regex':query, '$options':'i'}},
                 {'performer':{'$regex':query, '$options':'i'}}
-            ]}
-            ]},
-    )
+            ]}]},
+        { 'score': { '$meta': 'textScore' } }).sort([('score', {'$meta': 'textScore'})])
 
 
 async def prepare_index():
